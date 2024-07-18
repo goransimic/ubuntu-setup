65a549d36d9817d52c01bf5a3ed92175cf0db1b5
Goran Simic <goransimicdev@gmail.com>
2024-07-18 14:41:24 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo apt-get update > /dev/null
 sudo apt-get install -y git > /dev/null
 mkdir -p $HOME/.local/{bin,share}
 rm -rf $HOME/.local/share/ubuntu-setup
-git clone -q https://github.com/goransimicdev/ubuntu-setup.git $HOME/.local/share/ubuntu-setup
+git clone https://github.com/goransimicdev/ubuntu-setup.git $HOME/.local/share/ubuntu-setup
 ln -sf $HOME/.local/share/ubuntu-setup/setup.sh $HOME/.local/bin/ubuntu-setup
