3f82c2af9bf036a6a331640d4caa9b630dabb138
Goran Simic <goransimicdev@gmail.com>
2025-11-28 10:43:51 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo "Installing Ubuntu Setup..."
 sudo apt-get update > /dev/null
 sudo apt-get install -y git > /dev/null
-mkdir -p $HOME/.local/{bin,share}
-rm -rf $HOME/.local/{bin,share}/ubuntu-setup
-git clone https://github.com/goransimic/ubuntu-setup.git $HOME/.local/share/ubuntu-setup
-ln -sv $HOME/.local/share/ubuntu-setup/setup.sh $HOME/.local/bin/ubuntu-setup
+git clone https://github.com/goransimic/ubuntu-setup.git /tmp/ubuntu-setup
+cd /tmp/ubuntu-setup
+./setup.sh $1
+cd - > /dev/null
+rm -rf /tmp/ubuntu-setup
